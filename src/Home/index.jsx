57fadb3d263ca6f0263a4dc06aa42368909c9880
Deeter <deeter.cesler@gmail.com>
2020-10-21 17:06:46 -0600

    mobile friendly table, switch table out for <div>s + bootstrap cols

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -48,15 +48,12 @@ const Home = (props) => {
           { props.results !== null 
             &&
             (<div className="results">
-              <table>
-                <tbody>
-                  <tr className="row bold">
-                    <td></td>
-                    <td>Name</td>
-                    <td>Description</td>
-                    <td>Type</td>
-                    <td>Website</td>
-                  </tr>
+                  <div className="row bold desktop-only">
+                    <div className="col">Name</div>
+                    <div className="col">Description</div>
+                    <div className="col">Type</div>
+                    <div className="col">Website</div>
+                  </div>
                   {
                   props.results.length === 0 
                   &&
@@ -69,16 +66,13 @@ const Home = (props) => {
                     let website;
                     if(result[4] && result[4].substring(0,4) === "http") website = result[4];
                     else if (result[4]) website = "http://" + result[4];
-                    return <tr className="row result" key={result._id}>
-                      <td></td>
-                      <td><strong>{result[0]}</strong></td>
-                      <td>{result[1]}</td>
-                      <td>{result[3]}</td>
-                      <td className="website"><a rel="noopener noreferrer" target="_blank" href={website}>{result[4]}</a></td>
-                    </tr>
+                    return <div className="row result" key={result + Math.random()}>
+                      <div className="col-md"><strong>{result[0]}</strong></div>
+                      <div className="col-md">{result[1]}</div>
+                      <div className="col-md">{result[3]}</div>
+                      <div className="website col-md"><a rel="noopener noreferrer" target="_blank" href={website}>{result[4]}</a></div>
+                    </div>
                   })}
-                </tbody>
-              </table>
             </div>)
           }
         </div>
